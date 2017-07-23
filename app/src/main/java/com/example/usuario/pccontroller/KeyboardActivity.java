53fd1e9612c975715ab6d5647a3f0aa855b46cbf
Ian Duran <ianduran60@gmail.com>
2017-07-23 16:20:09 -0600

    Further customization of the keyboard Added flags to make the activity fully immersive Changed Min SDK to 19

--- app/src/main/java/com/example/usuario/pccontroller/KeyboardActivity.java
+++ app/src/main/java/com/example/usuario/pccontroller/KeyboardActivity.java
@@ -17,7 +17,12 @@ public class KeyboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_keyboard_layout);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         keyboard = new Keyboard(this, R.xml.qwerty);
         keyboardView = (KeyboardView)findViewById(R.id.custom_keyboard);
         keyboardView.setKeyboard(keyboard);
